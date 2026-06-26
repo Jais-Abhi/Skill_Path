6d1c75041fc6e1f6d480b0e14b187bcf6fa91fff
Abhishek Jaiswal <abhijaiswal7479@gmail.com>
2026-06-26 14:18:23 +0530

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import Layout from './components/Layout'
 import Landing from './pages/Landing'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -9,10 +10,12 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </Router>
   )
